b39dd0c6816046d867ff405bdd7a3bbd82eab603
Itzsammi <bodmas50@gmail.com>
2024-01-22 22:47:43 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
-
     try:
         result = a / b
-        print("Inside result: {:.1}".format(result))
-    except:
+    except ZeroDivisionError:
         result = None
-        print("Inside result: {}".format(result))
     finally:
-        return result
+        print("Inside result: {}".format(result))
+    return result
