f672c244420f993b9a3494f5778d2ccf7a71f9c6
Itzsammi <bodmas50@gmail.com>
2024-01-22 22:46:07 -0800

    100-safe_print_integer_err.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
+
     try:
         result = a / b
-        print("Inside result: {:.1f}".format(result))
+        print("Inside result: {:.1}".format(result))
     except:
         result = None
         print("Inside result: {}".format(result))
